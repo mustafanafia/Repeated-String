95b560bb8b23f73c8ae0065cdecf1d825b62c016
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-02-29 15:49:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) {
+
+        long total_full_strings = n / s.length();
+        long remainder = n % s.length();
+        long count = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a')
+                count++;
+        }
+        count = total_full_strings * count;
+        for(int i = 0; i < remainder; i++){
+            if(s.charAt(i) == 'a')
+                count++;
+        }
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
